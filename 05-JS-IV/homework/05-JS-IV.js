0aa834550355961abc5d646a45f658c9efee9cf0
Andres Herrera <jorandh@hotmail.com>
2021-10-07 12:01:31 -0500

    agregadas tareas varias

---
+++ 05-JS-IV/homework/05-JS-IV.js
@@ -0,0 +1,10 @@
+function tarea3(){
+    console.log("Objetos son contenedores de datos pero tienen propiedades y pueden tener métodos");
+    console.log("Propiedades son las que caracterizan al objeto que las contiene");
+    console.log("Métodos son funciones que están dentro de los objetos y cumplen tareas que se le asignen");
+    console.log("Bucle For In es un bucle para recorrer propiedades específicas de un objeto ");
+    console.log("la notación de puntos es más práctica ya que omite tener que escribir los corchetes cuadrados ");
+    
+    
+}
+tarea3
