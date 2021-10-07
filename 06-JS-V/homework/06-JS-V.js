142da06160a771678d9996b0871dea62c3fe57e2
Andres Herrera <jorandh@hotmail.com>
2021-10-07 12:02:41 -0500

    agregadas tareas varias

---
+++ 06-JS-V/homework/06-JS-V.js
@@ -0,0 +1,9 @@
+function tarea4(){
+    console.log("Prototype sirve para economizar memoria ya que una clase puede acceder a metodos que ya están creados");
+    console.log("en objetos superiores.");
+    console.log("Constructors son métodos que sirven para crear e inicializar objetos desde una clase");
+    
+    
+    
+}
+tarea4
