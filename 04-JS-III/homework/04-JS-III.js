be470ee31fb680a3416d1d362f41709621fa9333
Andres Herrera <jorandh@hotmail.com>
2021-10-07 11:59:39 -0500

    agregadas tareas varias

---
+++ 04-JS-III/homework/04-JS-III.js
@@ -0,0 +1,7 @@
+function tarea2(){
+    console.log("Arrays son datos guardados en una variable de tipo array. ");
+    console.log("Son guardados ordenadamente y cada uno ocupa una posición dentro de la variable tipo array.");
+    console.log("Se puede acceder a ellos individualmente, se pueden insertar datos, eliminar datos");
+    
+}
+tarea2
